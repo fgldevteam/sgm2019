263453b366951d5da46988aad5dc23228ad5e4c8
Brent Garner <bgarner@gmail.com>
2019-01-18 10:39:12 -0700

    most recent scores

--- scoreboard/scores.js
+++ scoreboard/scores.js
@@ -4,147 +4,147 @@ var scores = [
         "class": "avengers",
         "colour": "#6a5acd",
         "logo": "allens_avengers.svg",
-        "score": 2
+        "score": 33
     },
     {
         "teamName": "Bergens Bison",
         "class": "bison",
         "colour": "#932230",
         "logo": "bergens_bison.svg",
-        "score": 2
+        "score": 38
     },
     {
         "teamName": "Bradleys Blitz",
         "class": "blitz",
         "colour": "#4D74C1",
         "logo": "bradleys_blitz.svg",
-        "score": 3
+        "score": 34
     },
     {
         "teamName": "Browns Bruins",
         "class": "brown",
         "colour": "#593800",
         "logo": "Browns-Bruins.svg",
-        "score": 2
+        "score": 31
     }, 
     {
         "teamName": "Braunds Beast",
         "class": "beast",
         "colour": "#4b4b4b",
         "logo": "braunds_beast.svg",
-        "score": 5
+        "score": 42
     },
     {
         "teamName": "Dekkers Dragons",
         "class": "dragons",
         "colour": "#FF8835",
         "logo": "dekkers_dragons.svg",
-        "score": 3
+        "score": 26
     },
     {
         "teamName": "Friesens Fugitives",
         "class": "fugitives",
         "colour": "#BAC9C1",
         "logo": "friesens_fugitives.svg",
-        "score": 3
+        "score": 42
     },
     {
         "teamName": "Haennels Hurricanes",
         "class": "haennel",
         "colour": "#6b01a0",
         "logo": "Haennels-Hurricanes.svg",
-        "score": 2
+        "score": 30
     },     
     {
         "teamName": "Kotsopoulos Cobras",
         "class": "cobras",
         "logo": "kotsopoulos_cobras.svg",
         "colour": "#ff5602",
-        "score": 3
+        "score": 39
     },
     {
         "teamName": "Legomskis Lumberjacks",
         "class": "Lumberjacks",
         "colour": "#FFE892",
         "logo": "legomskis_lumberjacks.svg",
-        "score": 2
+        "score": 35
     },
     {
         "teamName": "Mackinnons Mammoths",
         "class": "mammoths",
         "colour": "#ff1f06",
         "logo": "mackinnons_mammoths.svg",
-        "score": 3
+        "score": 17
     },
     {
         "teamName": "Marinos Mayhem",
         "class": "marino",
         "colour": "#0058a0",
         "logo": "Marinos-Mayhem.svg",
-        "score": 3
+        "score": 40
     },  
     {
         "teamName": "McEacherns Mustangs",
         "class": "mustangs",
         "colour": "#b0c784",
         "logo": "mceacherns_mustangs.svg",
-        "score": 3
+        "score": 38
     },
     {
         "teamName": "Morales-Ryans Monsters",
         "class": "monsters",
         "colour": "#ff3329",
         "logo": "morales-ryans_monsters.svg",
-        "score": 9
+        "score": 51
     },
     {
         "teamName": "Rangers Rampage",
         "class": "Rampage",
         "colour": "#fce38f",
         "logo": "rangers_rampage.svg",
-        "score": 3
+        "score": 33
     },
     {
         "teamName": "Salewskis Swarm",
         "class": "swarm",
         "colour": "#FF992F",
         "logo": "salewskis_swarm.svg",
-        "score": 3
+        "score": 41
     },
     {
         "teamName": "Toerings Tigers",
         "class": "tigers",
         "colour": "#FFA438",
         "logo": "toerings_tigers.svg",
-        "score": 3
+        "score": 30
     },
     {
         "teamName": "Vandewals Vipers",
         "class": "vipers",
         "colour": "#24928e",
         "logo": "vandewals_vipers.svg",
-        "score": 4
+        "score": 20
     },
     {
         "teamName": "Woods Wolfpack",
         "class": "wolfpack",
         "colour": "#4b4b4b",
         "logo": "woods_wolfpack.svg",
-        "score": 3
+        "score": 41
     },
     {
         "teamName": "Granges Gladiators",
         "class": "gladiators",
         "colour": "#991000",
         "logo": "granges_gladiators.svg",
-        "score": 14
+        "score": 55
     },
     {
         "teamName": "Owens Owls",
         "class": "owls",
         "colour": "#E04964",
         "logo": "owens_owls.svg",
-        "score": 7
+        "score": 44
     },
 
     {
@@ -152,7 +152,7 @@ var scores = [
         "class": "rhinos",
         "colour": "#70c1ff",
         "logo": "reynolds_rhinos.svg",
-        "score": 7
+        "score": 27
     },    
 
     {
@@ -160,6 +160,6 @@ var scores = [
         "class": "phoenix",
         "colour": "#011d99",
         "logo": "philips_phoenix.svg",
-        "score": 3
+        "score": 30
     },      
 ];
